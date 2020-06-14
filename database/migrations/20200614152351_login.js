c8448d1a900a263ef684cc8b86eee76c4b327a1b
Horacio <hraciosapato2015hcs@gmail.com>
2020-06-14 16:51:27 +0300

    Added loging and User migrations

--- database/migrations/20200614152351_login.js
+++ database/migrations/20200614152351_login.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('login',function(table){
+      table.increments().primary()
+      table.string('email',255).notNullable()
+      table.string('password',100).notNullable()
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('login')
 };
